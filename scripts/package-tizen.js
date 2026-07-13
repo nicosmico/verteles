f78d802a08b5340dcbe98d42dad53dde9178f668
nicosmico <niccolas.dev@gmail.com>
2026-07-12 20:09:31 -0400

    clean: mover config.xml a public/ para que Vite lo copie automaticamente

--- scripts/package-tizen.js
+++ scripts/package-tizen.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 const rootDir = path.resolve(__dirname, '..');
 const distDir = path.join(rootDir, 'dist');
-const configXml = path.join(rootDir, 'config.xml');
+const configXml = path.join(rootDir, 'public', 'config.xml');
 const wgtPath = path.join(rootDir, 'verteles.wgt');
 
 function main() {
